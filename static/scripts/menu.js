397e54c15be7106ffcd1e85148741d46abf7c915
Sid <Sid@.(none)>
2008-12-28 22:30:58 +0530

    Added a shared menu and resume section. Updated the about me section a little.

---
+++ static/scripts/menu.js
@@ -0,0 +1,12 @@
+document.writeln("<div id=\"titlebar\">");
+document.writeln("<div id=\"title-content\" >");
+document.writeln("<a href=\"index.html\"><img src=\"images/logo.gif\" alt=\"Siddharth Mitra\" id=\"logo\"/> </a>");
+document.writeln("<H2>&nbsp;&nbsp;&nbsp;&nbsp; ");
+document.writeln("<A href=\"index.html\" title=\"Back to square one...\">Home</A> &nbsp;&nbsp;&nbsp;&nbsp; ");
+document.writeln("<A href=\"index.html\" title=\"Coming soon...\">Projects</A> &nbsp;&nbsp;&nbsp;&nbsp;");
+document.writeln("<A href=\"resume.pdf\" title=\"My resumé in a PDF format\">Resumé</A> &nbsp;&nbsp;&nbsp;&nbsp; ");
+document.writeln("<A href=\"books.html\" title=\"Books I'm reading.\">Reading List</A>");
+document.writeln("</H2>");
+document.writeln("</div>");
+document.writeln("</div>");
+
