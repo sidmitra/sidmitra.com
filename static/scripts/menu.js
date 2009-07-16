aae5952fe43c8f5c14ea2f8d3ee1b63600f1c286
Sid <sidmitra.del@gmail.com>
2009-07-16 10:30:38 +0530

    Added thesis and www09 pdf and removed extra css + mootools

--- static/scripts/menu.js
+++ static/scripts/menu.js
@@ -1,9 +1,9 @@
 document.writeln("<div id=\"titlebar\">");
 document.writeln("<div id=\"title-content\" >");
-document.writeln("<a href=\"index.html\"><img src=\"images/logo.gif\" alt=\"Siddharth Mitra\" id=\"logo\"/> </a>");
+document.writeln("<a href=\"index.html\"><img src=\"images/logo.gif\" alt=\"Siddharth Mitra\" id=\"logo\"/></a>");
 document.writeln("<H2>&nbsp;&nbsp;&nbsp;&nbsp; ");
 document.writeln("<A href=\"index.html\" title=\"Back to square one...\">Home</A> &nbsp;&nbsp;&nbsp;&nbsp; ");
-document.writeln("<A href=\"projects.html\" title=\"Publications and other docs\">Research</A> &nbsp;&nbsp;&nbsp;&nbsp; ");
+document.writeln("<A href=\"research.html\" title=\"Publications and other docs\">Research</A> &nbsp;&nbsp;&nbsp;&nbsp; ");
 document.writeln("Projects[offline] &nbsp;&nbsp;&nbsp;&nbsp;");
 document.writeln("<A href=\"resume.pdf\" title=\"My resumé in a PDF format\">Resumé</A> &nbsp;&nbsp;&nbsp;&nbsp; ");
 document.writeln("<A href=\"books.html\" title=\"Books I'm reading\">Reading List</A>");
